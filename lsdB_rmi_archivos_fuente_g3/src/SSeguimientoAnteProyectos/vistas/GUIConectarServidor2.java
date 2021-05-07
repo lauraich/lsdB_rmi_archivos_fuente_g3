06cb5ed3c43175ebe0bb7e401dd553da15bd7baf
lauraich <65029586+lauraich@users.noreply.github.com>
2021-05-06 21:17:09 -0500

    GUI registrar Usuario completa

--- lsdB_rmi_archivos_fuente_g3/src/SSeguimientoAnteProyectos/vistas/GUIConectarServidor2.java
+++ lsdB_rmi_archivos_fuente_g3/src/SSeguimientoAnteProyectos/vistas/GUIConectarServidor2.java
@@ -40,7 +40,7 @@ public class GUIConectarServidor2 extends javax.swing.JFrame {
         lblFondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(425, 341));
+        setPreferredSize(new java.awt.Dimension(425, 370));
 
         jPanel1.setPreferredSize(new java.awt.Dimension(980, 390));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -78,7 +78,7 @@ public class GUIConectarServidor2 extends javax.swing.JFrame {
         jPanel1.add(btnConectarS2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 280, -1, -1));
 
         lblFondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/fondo.png"))); // NOI18N
-        jPanel1.add(lblFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 420, 330));
+        jPanel1.add(lblFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 420, 350));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -90,9 +90,7 @@ public class GUIConectarServidor2 extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 352, Short.MAX_VALUE)
         );
 
         pack();
